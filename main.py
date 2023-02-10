12cc65c725de762fdbcc248c9d061c73219fcd23
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-02-10 20:11:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class menu:
 				print('''1.niat sholat''')
 				usna = input('masukan :')
 				if usna == '1':
-					import niatsholat from niatsholat
+					import niatsholat from test
 					print('import error')
 				elif usna == '2'
 					exit()
