5e78edec490908b477c2a57b3bf8e9fba6c861ca
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-02-10 20:08:45 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os,sys,time,json,requests
+
+class menu:
+				print('''1.niat sholat''')
+				usna = input('masukan :')
+				if usna == '1':
+					import test from niatsholat
+					print('import error')
+				elif usna == '2'
+					exit()
+					
+if __name__=='__main__':
+	menu()
